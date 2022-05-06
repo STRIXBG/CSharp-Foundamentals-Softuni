1cc8315c3cebe9ce90803329a29fa9c5ae046962
STRIXBG <65187330+STRIXBG@users.noreply.github.com>
2022-05-06 15:36:17 +0300

    Update exercise.cs

--- Basic Syntax, Conditional Statements and Loops - Lab/Student Information/exercise.cs
+++ Basic Syntax, Conditional Statements and Loops - Lab/Student Information/exercise.cs
@@ -1,11 +1,8 @@
 // See https://aka.ms/new-console-template for more information
-string studentName;
-int age;
-double studentGrade;
 Console.WriteLine("Enter student's Name: ");
-studentName = Console.ReadLine();
+string studentName = Console.ReadLine();
 Console.WriteLine("Enter student's Age: ");
-age = int.Parse(Console.ReadLine());
+int age = int.Parse(Console.ReadLine());
 Console.WriteLine("Enter student's Grade: ");
-studentGrade = int.Parse(Console.ReadLine());
+double studentGrade = double.Parse(Console.ReadLine());
 Console.WriteLine("Name: {0}, Age: {1}, Grade: {2:F2}", studentName,age,studentGrade);
